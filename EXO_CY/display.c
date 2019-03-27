bfe1c086f1188434e6f1e4194f2cc165256695cf
kiki <thiphan94@gmail.com>
2019-03-27 02:13:41 +0100

    commit code circle simple

--- EXO_CY/display.c
+++ EXO_CY/display.c
@@ -21,15 +21,43 @@ void display(void) {
   glRotatef(xRotation, 1, 0, 0);
   glRotatef(yRotation, 0, 1, 0);
   glScalef(2.0, 2.0, 2.0);
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  //
+  // glBegin(GL_POLYGON);
+  // for (int i = 0; i < 1000; i = i + 1) {
+  //   glColor3f(1.0f, 0.0f, 0.0f); // Red
+  //   float a = cos(2 * 3.14159 * i / 1000.0);
+  //   float b = sin(2 * 3.14159 * i / 1000.0);
+  //   glBegin(GL_LINES);
+  //   glColor3f(0.0f, 1.0f, 0.0f);
+  //   glVertex3f(a, b, 0);
+  //   glVertex3f(a, b, 1);
+  //   glEnd();
+  // }
+  // glEnd();
 
-  glBegin(GL_POINTS);
-  for (int i = 0; i < 1000; i = i + 1) {
-    float a = cos(2 * 3.14159 * i / 1000.0);
-    float b = sin(2 * 3.14159 * i / 1000.0);
-    glVertex3f(a, b, 0);
+  // ex
+
+  GLfloat ballRadius = 0.5f; // Radius of the bouncing ball
+  // glBegin(GL_TRIANGLE_FAN);
+  // glColor3f(0.0f, 0.0f, 1.0f);  // Blue
+  // glVertex3f(0.0f, 0.0f, 0.0f); // Center of circle
+  int numSegments = 10;
+  GLfloat angle;
+  glBegin(GL_LINE_LOOP);
+  for (int i = 0; i <= numSegments; i++) { // Last vertex same as first vertex
+    glColor3f(1.0f, 0.0f, 0.0f);           // Red
+    // glBegin(GL_LINES);
+
+    glColor3f(0.0f, 1.0f, 0.0f);         // Green
+    angle = i * 2.0f * PI / numSegments; // 360 deg for all segments
+    double a, b;
+    a = cos(angle) * ballRadius;
+    b = sin(angle) * ballRadius;
+    glVertex3f(a, b, 0.0);
+    glVertex3f(a, b, 1.0);
   }
   glEnd();
+  // glEnd();
 
   glPopMatrix();
 
