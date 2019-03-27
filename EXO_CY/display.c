6b34e69ccdfcd65b87b13bb5c49a73a9e7212265
kiki <thiphan94@gmail.com>
2019-03-27 03:28:25 +0100

    cylindre 3D

--- EXO_CY/display.c
+++ EXO_CY/display.c
@@ -8,6 +8,27 @@
 float xRotation = 0.0, yRotation = 0.0;
 float zTranslate = -3.0;
 
+//*****fonction draw_circle****//
+void draw_circle(int numSegments, GLfloat ballRadius, double z) {
+  GLfloat angle;
+  glBegin(GL_POLYGON);
+  for (int i = 0; i <= numSegments; i++) {
+    // Last vertex same as first vertex
+    if (z == 0.0) {
+      glColor3f(1.0f, 1.0f, 1.0f); // blanc
+    } else {
+      glColor3f(1.0f, 1.0f, 0.0f); // JAUNE
+    }
+
+    angle = i * 2.0f * PI / numSegments; // 360 deg for all segments
+    double a, b;
+    a = cos(angle) * ballRadius;
+    b = sin(angle) * ballRadius;
+    glVertex3f(a, b, z);
+  }
+  glEnd();
+}
+
 void display(void) {
 
   glClear(GL_COLOR_BUFFER_BIT);
@@ -21,44 +42,29 @@ void display(void) {
   glRotatef(xRotation, 1, 0, 0);
   glRotatef(yRotation, 0, 1, 0);
   glScalef(2.0, 2.0, 2.0);
-  //
-  // glBegin(GL_POLYGON);
-  // for (int i = 0; i < 1000; i = i + 1) {
-  //   glColor3f(1.0f, 0.0f, 0.0f); // Red
-  //   float a = cos(2 * 3.14159 * i / 1000.0);
-  //   float b = sin(2 * 3.14159 * i / 1000.0);
-  //   glBegin(GL_LINES);
-  //   glColor3f(0.0f, 1.0f, 0.0f);
-  //   glVertex3f(a, b, 0);
-  //   glVertex3f(a, b, 1);
-  //   glEnd();
-  // }
-  // glEnd();
 
-  // ex
-
+  // Declaration
   GLfloat ballRadius = 0.5f; // Radius of the bouncing ball
-  // glBegin(GL_TRIANGLE_FAN);
-  // glColor3f(0.0f, 0.0f, 1.0f);  // Blue
-  // glVertex3f(0.0f, 0.0f, 0.0f); // Center of circle
-  int numSegments = 10;
-  GLfloat angle;
-  glBegin(GL_LINE_LOOP);
-  for (int i = 0; i <= numSegments; i++) { // Last vertex same as first vertex
-    glColor3f(1.0f, 0.0f, 0.0f);           // Red
-    // glBegin(GL_LINES);
 
-    glColor3f(0.0f, 1.0f, 0.0f);         // Green
-    angle = i * 2.0f * PI / numSegments; // 360 deg for all segments
+  int numSegments = 1000;
+  GLfloat angle;
+  for (int k = 0; k <= numSegments; k++) {
+    // Last vertex same as first vertex
+
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue
+
+    angle = k * 2.0f * PI / numSegments; // 360 deg for all segments
     double a, b;
     a = cos(angle) * ballRadius;
     b = sin(angle) * ballRadius;
+    glBegin(GL_LINES);
     glVertex3f(a, b, 0.0);
     glVertex3f(a, b, 1.0);
+    glEnd();
   }
-  glEnd();
-  // glEnd();
 
+  draw_circle(numSegments, ballRadius, 0.0);
+  draw_circle(numSegments, ballRadius, 1.0);
   glPopMatrix();
 
   glutSwapBuffers();
